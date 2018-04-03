673d442562d98ca462dac8f8744201eb876c0d88
Md Asaduzzaman <oel.shakil@gmail.com>
2018-04-03 06:54:48 +0000

    Grayscale added to app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require turbolinks
+//= require popper
+//= require bootstrap-sprockets
+//= require jquery.easing
 //= require_tree .
